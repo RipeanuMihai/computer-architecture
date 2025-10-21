ae726a2c5af3e4d9aa7374aab11c33d4afd34521
Ripeanu Mihai <ripeanu.mihai@yahoo.com>
2025-10-21 12:52:00 +0300

    Test

--- chapters/rhdl/comb-skel/src/helper.rs
+++ chapters/rhdl/comb-skel/src/helper.rs
@@ -12,12 +12,12 @@ pub fn or_gate(_cr: ClockReset, i: (Bits<U1>, Bits<U1>)) -> Bits<U1> {
 
 #[kernel]
 pub fn and_gate(_cr: ClockReset, _i: (Bits<U1>, Bits<U1>)) -> Bits<U1> {
-    Bits::<U1>::default() // TODO add an and gate
+    i.1 & i.0
 }
 
 #[kernel]
-pub fn xor_gate(_cr: ClockReset, _i: (Bits<U1>, Bits<U1>)) -> Bits<U1> {
-    Bits::<U1>::default() // TODO add an xor gate
+pub fn xor_gate(_cr: ClockReset, i: (Bits<U1>, Bits<U1>)) -> Bits<U1> {
+    i.1 ^ i.0
 }
 
 
